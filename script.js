4383b5aafc09782b1464e5cdae5aea43e30446dd
taskinoz <tristan@taskinoz.com>
2018-01-21 02:17:08 +1100

    Fixed UL height again

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   //Set the UL height to 70% of the window
-  $('ul').height(window.innerHeight*0.65)
+  $('ul').height(window.innerHeight*0.62)
 
   var spiner = false;
   var rand = function (r){return Math.floor(Math.random()*r);}
